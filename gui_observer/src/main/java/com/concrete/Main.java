288bd455e98be69e61a947e6b5fbe172f7baf465
omerkeskin <omer.keskin.deu@hotmail.com>
2019-02-08 00:07:58 +0300

    Little observer - listener example.

--- gui_observer/src/main/java/com/concrete/Main.java
+++ gui_observer/src/main/java/com/concrete/Main.java
@@ -1,11 +1,47 @@
 package com.concrete;
 
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import listener.AngelListener;
+import listener.DevilListener;
+
+
+
+
+
 public class Main {
 
+	JFrame frame;
+	
 	public static void main(String[] args) {
+		Main ex = new Main();
+		ex.go();
+	}
+	
+	public void go() {
+		frame = new JFrame();
+		JButton button = new JButton("Should i do it.");
+		button.addActionListener(new AngelListener());
+		button.addActionListener(new DevilListener());
+		frame.getContentPane().add(button);
+		frame.setBounds(0, 0, 500, 500);
+		frame.setEnabled(true);
 		
-		System.out.println("Hello");
-
+		
+		JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+ 
+        panel.add(button);
+ 
+        frame.add(panel);
+        frame.setSize(300, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 	}
 
 }
---
+++ gui_observer/src/main/java/listener/AngelListener.java
@@ -0,0 +1,14 @@
+package listener;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AngelListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("Dont do it , you might regret it.");
+
+	}
+
+}
